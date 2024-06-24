b804390bb35b8b828d9eaa131dc3c2f513a860c7
wfbm <william.balan.machado@gmail.com>
2024-06-24 16:09:53 -0300

    initial config

---
+++ Style.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1
+  },
+  background: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    width: '40px',
+    heigh: '10px'
+  }
+});
+
